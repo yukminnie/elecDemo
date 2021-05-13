e058d01d548cb41af90ef2d269f5c790ab2f2944
yukminnie <dadiaomin@126.com>
2021-05-13 10:03:50 +0800

    了解事件和窗口，尝试载入本地模板文件

---
+++ main.js
@@ -0,0 +1,24 @@
+// app 是管理应用程序生命周期用的
+// BrowserWindow 是创建还有控制应用窗口用的 ..
+const {
+    app,
+    BrowserWindow
+} = require('electron')
+
+// 暂时设置成全局变量，防止垃圾回收
+let win
+
+// 定义事件
+const createWindow = () => {
+        win = new BrowserWindow({
+            width: 960,
+            height: 600,
+            minWidth: 830,
+            minHeight: 500,
+            backgroundColor: '#f8f8f8'
+        })
+        win.loadURL(`file://${__dirname}/main.html`)
+    }
+    
+// 绑定事件
+app.on('ready', createWindow)
