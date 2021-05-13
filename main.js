00977d9e8361806277b45f56c74b514d92496a16
yukminnie <dadiaomin@126.com>
2021-05-13 10:13:47 +0800

    不同平台的窗口事件区别

--- main.js
+++ main.js
@@ -25,7 +25,26 @@ const createWindow = () => {
             backgroundColor: '#f8f8f8'
         })
         win.loadURL(`file://${__dirname}/main.html`)
+
+        // 关闭窗口时进行的操作，和下面的 all-closed 和 activate 联系
+        win.on('closed', () => {
+            win = null
+        })
     }
 
 // 绑定事件
 app.on('ready', createWindow)
+
+// 不是 mac 平台就完全退出
+app.on('window-all-closed', () => {
+    if(process.platform !== "darwin") {
+        app.quit()
+    }
+})
+
+// mac 平台可以重新激活
+app.on('activate', () => {
+    if(win = null) {
+        createWindow()
+    }
+})
